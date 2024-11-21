aaeda4046ce7a30273e8cb4c11cee1dcc0555ea7
yogesh-gloify <yogeshtiwari6260@gmail.com>
2024-11-21 16:40:48 +0530

    DemoApp

---
+++ app/src/main/java/com/example/demoapp/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.demoapp
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    lateinit var btnShowMsg: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        btnShowMsg = findViewById(R.id.btnShowMsg)
+
+        btnShowMsg.setOnClickListener {
+            Log.i("MainActivity", "Button clicked!")
+            Toast.makeText(this, "Button clicked!", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
